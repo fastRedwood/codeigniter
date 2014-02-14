11cf71c5daa88e8e7f55c96e58a895fd1c9c0226
zhouzy <zhouzy@bss360.com>
2014-02-14 12:02:06 +0800

    template_c

--- application/views/template_c/c5/df/3a/c5df3aeb4ae1236e48a748f437399feda7d0c353.file.index.html.php
+++ application/views/template_c/c5/df/3a/c5df3aeb4ae1236e48a748f437399feda7d0c353.file.index.html.php
@@ -1,45 +1,29 @@
-<<<<<<< HEAD
-<?php /* Smarty version Smarty-3.1.16, created on 2014-02-14 11:48:39
-=======
-<?php /* Smarty version Smarty-3.1.16, created on 2014-02-14 11:08:58
->>>>>>> 65d16e1234d82c8a2c0fb4d72ea43c8e1b2ce18e
+<?php /* Smarty version Smarty-3.1.16, created on 2014-02-14 11:55:52
          compiled from "application/views/template/index.html" */ ?>
-<?php /*%%SmartyHeaderCode:141192871752fd88ca414e25-02630249%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:54204701452fd93c8d53458-54207587%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     'c5df3aeb4ae1236e48a748f437399feda7d0c353' => 
     array (
       0 => 'application/views/template/index.html',
-<<<<<<< HEAD
       1 => 1392348735,
-=======
-      1 => 1392343308,
->>>>>>> 65d16e1234d82c8a2c0fb4d72ea43c8e1b2ce18e
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '141192871752fd88ca414e25-02630249',
+  'nocache_hash' => '54204701452fd93c8d53458-54207587',
   'function' => 
   array (
   ),
-<<<<<<< HEAD
-  'version' => 'Smarty-3.1.16',
-  'unifunc' => 'content_52fd88ca438325_74037363',
-=======
->>>>>>> 65d16e1234d82c8a2c0fb4d72ea43c8e1b2ce18e
   'variables' => 
   array (
     'test' => 0,
   ),
   'has_nocache_code' => false,
-<<<<<<< HEAD
-=======
   'version' => 'Smarty-3.1.16',
-  'unifunc' => 'content_52fd88ca438325_74037363',
->>>>>>> 65d16e1234d82c8a2c0fb4d72ea43c8e1b2ce18e
+  'unifunc' => 'content_52fd93c8d77236_42076243',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_52fd88ca438325_74037363')) {function content_52fd88ca438325_74037363($_smarty_tpl) {?><!DOCTYPE html>
+<?php if ($_valid && !is_callable('content_52fd93c8d77236_42076243')) {function content_52fd93c8d77236_42076243($_smarty_tpl) {?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
