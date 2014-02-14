3213eeac9d984f22d9ab528a98e811d47afc1cf5
zhouzy <zhouzy@bss360.com>
2014-02-14 11:49:32 +0800

    fix

---
+++ application/views/template_c/c5/df/3a/c5df3aeb4ae1236e48a748f437399feda7d0c353.file.index.html.php
@@ -0,0 +1,36 @@
+<?php /* Smarty version Smarty-3.1.16, created on 2014-02-14 11:48:39
+         compiled from "application/views/template/index.html" */ ?>
+<?php /*%%SmartyHeaderCode:141192871752fd88ca414e25-02630249%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'c5df3aeb4ae1236e48a748f437399feda7d0c353' => 
+    array (
+      0 => 'application/views/template/index.html',
+      1 => 1392348735,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '141192871752fd88ca414e25-02630249',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.16',
+  'unifunc' => 'content_52fd88ca438325_74037363',
+  'variables' => 
+  array (
+    'test' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_52fd88ca438325_74037363')) {function content_52fd88ca438325_74037363($_smarty_tpl) {?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>smarty配置测试</title>
+</head>
+<body>
+    <?php echo $_smarty_tpl->tpl_vars['test']->value;?>
+
+</body>
+</html><?php }} ?>
