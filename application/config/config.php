03de9021bdd409e63a109dac53360fa753da26ea
xiakai <livecho@sina.com>
2014-02-20 23:24:44 +0800

    echo

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.cicms.com/';
+$config['base_url']	= 'http://www.codeigniterdev.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://www.cicms.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
